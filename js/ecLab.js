7385f3fa61368848d2696622de2fc5e93ce9b7f7
andigandhi <andigrasser13@gmail.com>
2021-05-13 16:44:04 +0200

    Added some minimal JS functions

--- js/ecLab.js
+++ js/ecLab.js
@@ -1,4 +1,4 @@
-
+var methodes = [];
 
 function show(elem) {
     document.getElementById(elem+'Div').style.display = "inline";
@@ -10,14 +10,66 @@ function add(elem, save) {
     document.getElementById(elem+'Div').style.display = "none";
     document.getElementById('addButtonsDiv').style.display = "inline";
     if (save) {
-        addParamsToTable();
+        addParamsToTable(elem);
     }
 }
 
-function addParamsToTable() {
-    var tbl = document.getElementById('overviewTable');
+function addParamsToTable(methode) {
+    var outDiv = document.getElementById('overviewDiv');
 
-    var tblTd = document.createElement("td");
-    tblTd.innerHTML = "YOOOOOO";
-    tbl.appendChild(tblTd);
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = "<b>"+methode+"</b><br>";
+
+    outDiv.appendChild(newDiv);
+
+    addParamsToOut(methode)
+}
+
+function addParamsToOut(methode) {
+    if (methode == "BCD") addBcdToOut();
+
+    console.log(methodes);
+}
+
+function addBcdToOut() {
+    methodes[methodes.length] = 
+        `
+        Technique : `+(methodes.length+1)+`
+        Battery Capacity Determination
+        Set I/C             I                   
+        Is1                 260,000             
+        unit Is1            mA                  
+        N1                  1,00                
+        I1 sign             > 0                 
+        t1 (h:m:s)          11:00:0,0000        
+        EM1 (V)             0,400               
+        EM1 vs.             Ref                 
+        EM2 (V)             -0,200              
+        EM2 vs.             Ref                 
+        dE1 (mV)            0,00                
+        dt1 (s)             10,0000             
+        Hold NONE/EM1/EM2   1                   
+        tM (h:m:s)          1:00:0,0000         
+        Set I limit         0                   
+        Im                  130,000             
+        unit Im             mA                  
+        Nl                  20,00               
+        I Range             1 A                 
+        Bandwidth           5                   
+        E range min (V)     -2,500              
+        E range max (V)     2,500               
+        tR (h:m:s)          0:00:0,0000         
+        dER/dt (mV/h)       0,0                 
+        dER (mV)            0,00                
+        dtR (s)             0,0000              
+        Charge/Discharge    1                   
+        Set I/C             I                   
+        Is2                 -260,000            
+        unit Is2            mA                  
+        N2                  1,00                
+        I2 sign             > 0                 
+        t2 (h:m:s)          11:00:0,0000        
+        Return Ei           0                   
+        use C               1  
+        `;
 }
